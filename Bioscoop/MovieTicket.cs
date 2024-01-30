372974a7a5feb7d02c93bc5fc7dc0b63cf1129ed
Niekelodius <niekelodius@gmail.com>
2024-01-30 12:35:07 +0100

    added movieticket

--- Bioscoop/MovieTicket.cs
+++ Bioscoop/MovieTicket.cs
@@ -1,5 +1,27 @@
 ﻿namespace Bioscoop;
 
 public class MovieTicket {
-    
+    private int RowNr;
+    private int SeatNr;
+    private boolean isPremium;
+    private MovieScreening movieScreening;
+
+    public void MovieTicket(MovieScreening movieScreening, int RowNr, int SeatNr, boolean isPremium) {
+        this.movieScreening = movieScreening;
+        this.RowNr = RowNr;
+        this.SeatNr = SeatNr;
+        this.isPremium = isPremium;
+    }
+
+    public boolean isPremiumTicket() {
+        return this.isPremium;
+    }
+
+    public double getPrice() {
+        return this.movieScreening.getPricePerSeat();
+    }
+
+    public override string ToString() {
+        return this.movieScreening + " " + this.RowNr + " " + this.SeatNr + " " + this.isPremium;
+    }
 }
