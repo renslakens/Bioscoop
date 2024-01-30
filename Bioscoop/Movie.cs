319cd350758f59c7e665e7e3f29d2f1da54bfa79
Niekelodius <niekelodius@gmail.com>
2024-01-30 12:29:51 +0100

    added movie and moviescreening

--- Bioscoop/Movie.cs
+++ Bioscoop/Movie.cs
@@ -1,5 +1,18 @@
 ﻿namespace Bioscoop;
 
 public class Movie {
-    
-} 
+    private string title;
+    private MovieScreening[] screenings;
+
+    public Movie(string title) {
+        this.title = title;
+    }
+
+    public void addScreening(MovieScreening screening) {
+        this.screenings.add(screening);
+    }
+
+    public override string ToString() {
+        return this.title + " " + this.screenings;
+    }
+}
--- Bioscoop/MovieScreening.cs
+++ Bioscoop/MovieScreening.cs
@@ -1,5 +1,22 @@
 ﻿namespace Bioscoop;
 
 public class MovieScreening {
-    
+    private LocalDateTime dateAndTime;
+    private double pricePerSeat;
+    private Movie movie;
+    private MovieTicket[] tickets;
+
+    public void MovieScreening(Movie movie, LocalDateTime dateAndTime, double pricePerSeat) {
+        this.movie = movie;
+        this.dateAndTime = dateAndTime;
+        this.pricePerSeat = pricePerSeat;
+    }
+
+    public double getPricePerSeat() {
+        return this.pricePerSeat;
+    }
+
+    public override string ToString() {
+        return this.movie + " " + this.dateAndTime + " " + this.pricePerSeat;
+    }
 }
