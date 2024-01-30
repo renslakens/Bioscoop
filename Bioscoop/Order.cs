830d3b886681f91cd85388a6257bdfd97f70d76b
Rens Lakens <rens@lakens.org>
2024-01-30 12:13:14 +0100

    added Order.cs

--- Bioscoop/Order.cs
+++ Bioscoop/Order.cs
@@ -1,5 +1,27 @@
 ﻿namespace Bioscoop;
 
 public class Order {
+    private int orderNr;
+    private bool isStudentOrder;
+
+    public Order(int orderNr, bool isStudentOrder) {
+        this.orderNr = orderNr;
+        this.isStudentOrder = isStudentOrder;
+    }
     
+    public int GetOrderNr() {
+        return orderNr;
+    }
+    
+    public void addSeatReservation(MovieTicket ticket) {
+        
+    }
+
+    public double calculatePrice() {
+        return 0;
+    }
+
+    public void export(TicketExportFormat exportFormat) {
+        
+    }
 }
