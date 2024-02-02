12deb527ae66b51a35cc3c8fe8a1da161a7d932b
Niekelodius <niekelodius@gmail.com>
2024-02-02 12:30:15 +0100

    added three ticket test

--- Tests/OrderTest.cs
+++ Tests/OrderTest.cs
@@ -1,7 +1,28 @@
+using Bioscoop;
+using Xunit;
+
+
 namespace Tests;
 
 public class OrderTest {
     [Fact]
-    public void Test1() {
+    public void TestThreeTickets() {
+        Movie movie = new Movie("The Matrix");
+        MovieScreening screening = new MovieScreening(movie, DateTime.Now, 10.0);
+        MovieTicket ticket = new MovieTicket(screening, 1, 1, false);
+        MovieTicket ticket2 = new MovieTicket(screening, 2, 2, false);
+        MovieTicket ticket3 = new MovieTicket(screening, 3, 3, false);
+        Order order = new Order(1, true);
+        order.addSeatReservation(ticket);
+        order.addSeatReservation(ticket2);
+        order.addSeatReservation(ticket3);
+
+        order.export(TicketExportFormat.PLAINTEXT);
+        order.export(TicketExportFormat.JSON);
+
+        var price = order.calculatePrice();
+
+        // Assert
+        Assert.Equal(20, price);
     }
 }
